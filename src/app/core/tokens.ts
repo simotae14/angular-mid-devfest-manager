984c18e8b30000e4f7897ba808ee630f7c875df9
Simone Taeggi <startae14@gmail.com>
2026-06-06 23:51:01 +0200

    18 Injection Tokens & Factories, method 2 Factory Provider

--- src/app/core/tokens.ts
+++ src/app/core/tokens.ts
@@ -1,3 +1,11 @@
-import { InjectionToken } from '@angular/core';
+import { InjectionToken, inject } from '@angular/core';
 
 export const API_URL = new InjectionToken<string>('API_URL');
+
+export const TICKETS_URL = new InjectionToken<string>('TICKETS_URL', {
+  providedIn: 'root',
+  factory: () => {
+    const api = inject(API_URL);   // inietta un altro token
+    return `${api}/tickets`;
+  },
+});
